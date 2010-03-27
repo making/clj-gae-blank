47dc678034f28abfbf18df0a9561e7ec3edc352a
making <makingx@gmail.com>
2010-03-28 02:57:40 +0900

    add get-service

--- test/clj_gae_blank/ds.clj
+++ test/clj_gae_blank/ds.clj
@@ -1,11 +1,14 @@
 (ns clj_gae_blank.ds
-  (:use [clojure.test])
+  (:use [clojure.test]
+        [clojure.contrib.singleton])
   (:import [com.google.appengine.api.datastore 
             DatastoreServiceFactory DatastoreService 
             Entity Key Query]
            [com.google.appengine.tools.development.testing 
             LocalDatastoreServiceTestConfig 
             LocalServiceTestHelper]))
+
+(def get-service (global-singleton #(DatastoreServiceFactory/getDatastoreService)))
 
 (defmacro defdstest [test-name & body]
   `(deftest ~test-name
@@ -19,11 +22,10 @@
 
 
 (defdstest foo
-    (let [entity (Entity. "test")
-          _ (doto entity 
-              (.setProperty "prop1" 100)
-              (.setProperty "prop2" "str"))
-          service (DatastoreServiceFactory/getDatastoreService)
+    (let [entity (doto (Entity. "test")
+                   (.setProperty "prop1" 100)
+                   (.setProperty "prop2" "str"))
+          service (get-service)
           key (.put service entity)]
       (is (not (nil? key)))
       (is (= entity (.get service key)))))
